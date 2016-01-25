2d357336ce76af11d7139827343c131d569325ca
OmFGitzKidz <allenwaiker@hotmail.com>
2016-01-25 19:03:11 +0800

    Update Ver1.1

--- Assets/Scripts/LevelController.cs
+++ Assets/Scripts/LevelController.cs
@@ -1,6 +1,5 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
-using UnityEngine.SceneManagement;
 using System.Collections;
 
 public class LevelController : MonoBehaviour {
@@ -17,67 +16,78 @@ public class LevelController : MonoBehaviour {
 
 	//calling the main menu scene
 	public void loadMainMenu(){
-		SceneManager.LoadScene("Main Menu Page");
+		//SceneManager.LoadScene("Main Menu Page");
+        Application.LoadLevel("Main Menu Page");
 		Debug.Log ("main menu pressed");
 	}
 
 	//calling the tutorial scene
 	public void loadTutorial(){
-		SceneManager.LoadScene("Tutorial Page");
-		Debug.Log ("tutorial pressed");
+		//SceneManager.LoadScene("Tutorial Page");
+        Application.LoadLevel("Tutorial Page");
+        Debug.Log ("tutorial pressed");
 	}
 
 	//calling the Options scene
 	public void loadOptions(){
-		SceneManager.LoadScene("Options Page");
+		//SceneManager.LoadScene("Options Page");
+        Application.LoadLevel("Options Page");
 		Debug.Log ("options pressed");
 	}
 
 	//calling the campaign scene
 	public void loadCampaign(){
-		SceneManager.LoadScene(""); //<------- need to update when scene is ready
+		//SceneManager.LoadScene(""); //<------- need to update when scene is ready
+        Application.LoadLevel("");
 		Debug.Log ("campaign pressed");
 	}
 
 	//calling the versus scene
 	public void loadVersus(){
-		SceneManager.LoadScene(""); //<------- need to update when scene is ready
+		//SceneManager.LoadScene(""); //<------- need to update when scene is ready
+        Application.LoadLevel("");
 		Debug.Log ("versus pressed");
 	}
 
 	//calling the weapons selection scene
 	public void loadWeaponSelect(){
-		SceneManager.LoadScene(""); //<------- need to update when scene is ready
+		//SceneManager.LoadScene(""); //<------- need to update when scene is ready
+        Application.LoadLevel("");
 		Debug.Log ("weapons selection pressed");
 	}
 
 	//calling the lose page scene
 	public void loadLosePage(){
-		SceneManager.LoadScene("");
+		//SceneManager.LoadScene("");
+        Application.LoadLevel("");
 		Debug.Log ("lose page");
 	}
 
 	//calling the win page scene
 	public void loadWinPage(){
-		SceneManager.LoadScene("");
+		//SceneManager.LoadScene("");
+        Application.LoadLevel("");
 		Debug.Log ("win page");
 	}
 
 	//calling the Achievemment page
 	public void loadAchievementsPage(){
-		SceneManager.LoadScene("");
+		//SceneManager.LoadScene("");
+        Application.LoadLevel("");
 		Debug.Log ("achievements page");
 	}
 
 	//calling the facebook page
 	public void loadFacebookPage(){
-		SceneManager.LoadScene("");
+		//SceneManager.LoadScene("");
+        Application.LoadLevel("");
 		Debug.Log ("quit pressed");
 	}
 
 	//calling the twitter page
 	public void loadTwitterPage(){
-		SceneManager.LoadScene("");
+		//SceneManager.LoadScene("");
+        Application.LoadLevel("");
 		Debug.Log ("quit pressed");
 	}
 
